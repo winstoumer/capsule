c55d33d7fa9fc1eeb42dbef85f8baf83c53de565
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-05-07 23:46:50 +0000

    modified:   capsule/src/components/ActiveTime/ActiveTime.tsx 	modified:   capsule/src/components/ActiveTime/activeTime.scss 	modified:   capsule/src/components/Navigation/navigation.scss 	modified:   capsule/src/index.css 	modified:   capsule/src/pages/home.scss

--- capsule/src/components/ActiveTime/ActiveTime.tsx
+++ capsule/src/components/ActiveTime/ActiveTime.tsx
@@ -3,7 +3,14 @@ import './activeTime.scss';
 export const ActiveTime = () => {
 
     return <div className='active-time'>
-      <div className='name-for'>Time left</div>
-      <div className='time-left'>00:00:00</div>
+        <div className='name-for'>
+            Time left
+        </div>
+        <div className='time-left'>
+            00:00:00
+        </div>
+        <div className='info-for color-yellow'>
+            (100/h)
+        </div>
     </div>
 };
