3ec311eb94e1ba9081c52cdb6bd865c12402390a
winstoumer <winstoumer@proton.me>
2024-05-10 19:53:03 +0200

    modified:   capsule/src/components/Nft/Nft.tsx 	modified:   capsule/src/components/Nft/nft.scss 	modified:   capsule/src/index.css

--- capsule/src/components/Nft/Nft.tsx
+++ capsule/src/components/Nft/Nft.tsx
@@ -4,9 +4,14 @@ export const Nft = () => {
 
     return <div className='default-page nft-container'>
         <img src="/preview_nft.png" className='preview-nft' />
-        <h3>
-            You have mined <span className='color-purple'>1</span> nft
-        </h3>
-        <button className='default-button'>Mint for 0.5 TON</button>
+        <div className='nft-description'>
+            <div className='nft-preview-title'>
+                You have mined <span className='color-purple'>1</span> nft
+            </div>
+            <div className='price-mint'>
+                <span className='color-blue'>0.5</span> TON
+            </div>
+            <button className='default-button'>Mint</button>
+        </div>
     </div>
 };
