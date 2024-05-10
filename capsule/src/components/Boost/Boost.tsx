016400f1a6f5b0ff34ce3fc16d21a2ffc6d17cca
winstoumer <winstoumer@proton.me>
2024-05-10 22:47:23 +0200

    modified:   capsule/src/components/Boost/Boost.tsx

--- capsule/src/components/Boost/Boost.tsx
+++ capsule/src/components/Boost/Boost.tsx
@@ -1,30 +1,55 @@
 import './boost.scss';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-export const Boost = () => {
+interface Level {
+    id: number;
+    name: string;
+    coins: number;
+    time: number;
+    mines_nft: boolean;
+}
+
+export const Boost: React.FC = () => {
+    const levels: Level[] = [
+        { id: 1, name: 'LvL 1', coins: 100, time: 1, mines_nft: false },
+        { id: 2, name: 'LvL 2', coins: 200, time: 4, mines_nft: true },
+        { id: 3, name: 'LvL 3', coins: 300, time: 6, mines_nft: true }
+    ];
+
+    const [currentLevel, setCurrentLevel] = useState(levels[0]);
     const [animate, setAnimate] = useState(false);
 
     const handleUpgrade = () => {
-        setAnimate(true); // Устанавливаем true, чтобы анимация проигралась
-        // Ваша логика обновления
+        const currentIndex = levels.findIndex(level => level.id === currentLevel.id);
+        if (currentIndex < levels.length - 1) {
+            setCurrentLevel(levels[currentIndex + 1]);
+            setAnimate(true);
+            setTimeout(() => {
+                setAnimate(false);
+            }, 500);
+        }
     };
 
-    return <div className='default-page evently-container'>
-        <div className='balance'>
-            120
-        </div>
-        <div className={`boost-container ${animate ? 'boost-container-animate' : ''}`}>
-            <div className='boost-item'>
-                <img src="capsule_v_2.png" className='boost-item-image' alt="Boost Item" />
+    return (
+        <div className='default-page evently-container'>
+            <div className='balance'>
+                {currentLevel.coins}
             </div>
-            <div className='boost-info'>
-                <div className='boost-name'>LvL 2</div>
-                <div className='boost-param'>200/h</div>
-                <div className='boost-param'>
-                    <span className='color-purple'>Mines NFT</span>
+            <div className={`boost-container ${animate ? 'boost-container-animate' : ''}`}>
+                <div className='boost-item'>
+                    <img src="capsule_v_2.png" className='boost-item-image' alt="Boost Item" />
                 </div>
+                <div className='boost-info'>
+                    <div className='boost-name'>{currentLevel.name}</div>
+                    <div className='boost-param'>{currentLevel.coins}/{currentLevel.time}h</div>
+                    {currentLevel.mines_nft && (
+                        <div className='boost-param'>
+                            <span className='color-purple'>Mines NFT</span>
+                        </div>
+                    )}
+                </div>
+                <button className='default-button' onClick={handleUpgrade}>Upgrade</button>
             </div>
-            <button className='default-button' onClick={handleUpgrade}>Upgrade</button>
         </div>
-    </div>
+    );
 };
