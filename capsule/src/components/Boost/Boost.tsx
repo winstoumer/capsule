8cd3d6d89487aaf502ad90d1e2259f2d555d7784
winstoumer <winstoumer@proton.me>
2024-05-10 23:14:39 +0200

    modified:   capsule/src/components/Boost/Boost.tsx 	modified:   capsule/src/components/Boost/boost.scss 	modified:   capsule/src/index.css

--- capsule/src/components/Boost/Boost.tsx
+++ capsule/src/components/Boost/Boost.tsx
@@ -1,5 +1,6 @@
-import './boost.scss';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './boost.scss';
 
 interface Level {
     id: number;
@@ -7,13 +8,15 @@ interface Level {
     coins: number;
     time: number;
     mines_nft: boolean;
+    price: number;
 }
 
 export const Boost: React.FC = () => {
+    const [balance, setBalance] = useState(1600); // Устанавливаем начальный баланс
     const levels: Level[] = [
-        { id: 1, name: 'LvL 1', coins: 100, time: 1, mines_nft: false },
-        { id: 2, name: 'LvL 2', coins: 200, time: 4, mines_nft: true },
-        { id: 3, name: 'LvL 3', coins: 300, time: 6, mines_nft: true }
+        { id: 1, name: 'LvL 1', coins: 100, time: 1, mines_nft: false, price: 160 },
+        { id: 2, name: 'LvL 2', coins: 200, time: 4, mines_nft: true, price: 340 },
+        { id: 3, name: 'LvL 3', coins: 300, time: 6, mines_nft: true, price: 1020 }
     ];
 
     const [currentLevel, setCurrentLevel] = useState(levels[0]);
@@ -22,18 +25,23 @@ export const Boost: React.FC = () => {
     const handleUpgrade = () => {
         const currentIndex = levels.findIndex(level => level.id === currentLevel.id);
         if (currentIndex < levels.length - 1) {
-            setCurrentLevel(levels[currentIndex + 1]);
-            setAnimate(true);
-            setTimeout(() => {
-                setAnimate(false);
-            }, 500);
+            if (balance >= levels[currentIndex + 1].price) {
+                setCurrentLevel(levels[currentIndex + 1]);
+                setBalance(balance - levels[currentIndex + 1].price); // Обновляем баланс после покупки
+                setAnimate(true);
+                setTimeout(() => {
+                    setAnimate(false);
+                }, 500);
+            } else {
+                alert('Недостаточно средств для покупки!');
+            }
         }
     };
 
     return (
         <div className='default-page evently-container'>
             <div className='balance'>
-                {currentLevel.coins}
+                {balance}
             </div>
             <div className={`boost-container ${animate ? 'boost-container-animate' : ''}`}>
                 <div className='boost-item'>
@@ -48,7 +56,14 @@ export const Boost: React.FC = () => {
                         </div>
                     )}
                 </div>
-                <button className='default-button' onClick={handleUpgrade}>Upgrade</button>
+                <div className='price-item'>
+                    <span className='color-blue'>{currentLevel.price}</span>
+                </div>
+                {balance >= currentLevel.price ? (
+                    <button className='default-button' onClick={handleUpgrade}>Upgrade</button>
+                ) : (
+                    <Link to="/" className='default-button'>Mine</Link>
+                )}
             </div>
         </div>
     );
