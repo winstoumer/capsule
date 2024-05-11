4f6c6f972c7e8248ff729abec220d33f1a6e39a9
winstoumer <winstoumer@proton.me>
2024-05-11 21:36:46 +0200

    modified:   capsule/src/components/Header/Header.tsx

--- capsule/src/components/Header/Header.tsx
+++ capsule/src/components/Header/Header.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
 import './header.scss';
 
+type TelegramUserData = {
+    id: number;
+    first_name: string;
+    last_name?: string;
+    username?: string;
+};
+
 export const Header = () => {
+    const [userData, setUserData] = useState<TelegramUserData | null>(null);
+
+    useEffect(() => {
+        if (window.Telegram.WebApp) {
+            setUserData(window.Telegram.WebApp.initDataUnsafe?.user);
+        }
+    }, []);
 
     return <header>
         <div className='header-width'>
             <div className="header-b">
-                Bob
+                {userData ? (
+                    <>
+                        {userData.first_name}
+                    </>
+                ) : (
+                    <></>
+                )}
             </div>
             <div className="header-b">
                 <button className='connect-wallet-button'>Connect wallet</button>
