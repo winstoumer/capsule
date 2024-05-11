d407009514a42118e2309766f253a42f554e79a9
winstoumer <winstoumer@proton.me>
2024-05-11 18:36:51 +0200

    modified:   capsule/src/components/Nft/Nft.tsx 	modified:   capsule/src/components/Nft/nft.scss

--- capsule/src/components/Nft/Nft.tsx
+++ capsule/src/components/Nft/Nft.tsx
@@ -12,6 +12,9 @@ export const Nft = () => {
                     <img src="/preview_nft_back.jpeg" />
                 </div>
             </div>
+            <div className='glow-card'>
+                <div className='glow-nft'></div>
+            </div>
         </div>
         <div className='nft-description'>
             <div className='nft-preview-title'>
