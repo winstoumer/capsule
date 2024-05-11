ae58cd7629d4d7b44255471745ab622535b7b8c7
winstoumer <winstoumer@proton.me>
2024-05-11 19:22:30 +0200

    modified:   capsule/src/components/ActiveTime/ActiveTime.tsx 	modified:   capsule/src/components/ActiveTime/activeTime.scss 	modified:   capsule/src/pages/HomePage.tsx 	modified:   capsule/src/pages/home.scss

--- capsule/src/components/ActiveTime/ActiveTime.tsx
+++ capsule/src/components/ActiveTime/ActiveTime.tsx
@@ -1,6 +1,16 @@
 import './activeTime.scss';
+import { useState, useEffect } from 'react';
 
 export const ActiveTime = () => {
+    const [activeText, setActiveText] = useState("Active..");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setActiveText(prevText => prevText === "Active.." ? "Search Nft.." : "Active..");
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return <div className='active-time'>
         <div className='time-left'>
@@ -8,6 +18,9 @@ export const ActiveTime = () => {
         </div>
         <div className='info-for'>
             100/h
+        </div>
+        <div className='active-signal'>
+            {activeText}
         </div>
     </div>
 };
--- capsule/src/pages/HomePage.tsx
+++ capsule/src/pages/HomePage.tsx
@@ -1,21 +1,12 @@
 // src/pages/HomePage.tsx
 import './home.scss';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Header } from "../components/Header/Header";
 import { Navigation } from "../components/Navigation/Navigation";
 import { ActiveTime } from "../components/ActiveTime/ActiveTime";
 import PageComponent from '../components/PageComponent/PageComponent';
 
 const HomePage: React.FC = () => {
-  const [activeText, setActiveText] = useState("Active..");
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveText(prevText => prevText === "Active.." ? "Search Nft.." : "Active..");
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <div className='content'>
@@ -27,9 +18,6 @@ const HomePage: React.FC = () => {
             <img src="/capsule_v_1.png" className='always-capsule' />
           </div>
           <ActiveTime />
-          <div className='active-signal'>
-            {activeText}
-          </div>
         </div>
         <Navigation />
       </PageComponent>
