75404559fc030d60554b51518718db1415f2c949
winstoumer <winstoumer@proton.me>
2024-05-11 16:57:27 +0200

    modified:   capsule/src/components/Referral/Referral.tsx 	modified:   capsule/src/components/Referral/referral.scss 	modified:   capsule/src/pages/home.scss

--- capsule/src/components/Referral/Referral.tsx
+++ capsule/src/components/Referral/Referral.tsx
@@ -3,10 +3,11 @@ import './referral.scss';
 export const Referral = () => {
 
     return <div className='referral-container'>
+        <div className='invited-count'>Invited 0</div>
         <div className='referral-watch'>
-            <div className='invited-count'>Invited 0</div>
+
             <img src="/invite.jpg" className='referral-image' />
-            <button className='default-button'>Referral link</button>
         </div>
+        <button className='default-button'>Referral link</button>
     </div>
 };
