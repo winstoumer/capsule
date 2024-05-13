9e4e48af28d6152ab1e520fa9ba4d25968af461d
winstoumer <winstoumer@proton.me>
2024-05-13 02:25:29 +0200

    modified:   capsule/src/components/ActiveTime/ActiveTime.tsx

--- capsule/src/components/ActiveTime/ActiveTime.tsx
+++ capsule/src/components/ActiveTime/ActiveTime.tsx
@@ -16,28 +16,6 @@ export const ActiveTime = () => {
     const [miningInfo, setMiningInfo] = useState<MiningData | null>(null);
     const [timeToNext, setTimeToNext] = useState<{ hours: number, minutes: number }>({ hours: 0, minutes: 0 });
 
-    const [currentTime, setCurrentTime] = useState<Date | null>(null);
-
-    useEffect(() => {
-        ntpClient.getNetworkTime("pool.ntp.org", 123, function (err: string | Error | null, date: Date | null) {
-            if (err || !date) {
-                console.error(err);
-                return;
-            }
-            setCurrentTime(date);
-        });
-    }, []);
-
-    const formatTimeUTC = (time: Date): string => {
-        const year = time.getUTCFullYear();
-        const month = (time.getUTCMonth() + 1).toString().padStart(2, '0'); // Месяц начинается с 0
-        const day = time.getUTCDate().toString().padStart(2, '0');
-        const hours = time.getUTCHours().toString().padStart(2, '0');
-        const minutes = time.getUTCMinutes().toString().padStart(2, '0');
-        const seconds = time.getUTCSeconds().toString().padStart(2, '0');
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    };    
-
     useEffect(() => {
         if (window.Telegram && window.Telegram.WebApp) {
             setUserData(window.Telegram.WebApp.initDataUnsafe?.user);
@@ -90,11 +68,7 @@ export const ActiveTime = () => {
             </div>
             <div className='active-time'>
                 <div>
-                    {currentTime ? (
-                        <div>Текущее время: {formatTimeUTC(currentTime)}</div>
-                    ) : (
-                        <div>Загрузка времени...</div>
-                    )}
+                    
                 </div>
                 <div className='time-left'>
                     {timeToNext.hours}h {timeToNext.minutes}m
