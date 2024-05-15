ca36247493913f8fa08f870509647b5ee0b27c75
winstoumer <winstoumer@proton.me>
2024-05-15 20:21:19 +0200

    modified:   capsule/src/components/Boost/Boost.tsx

--- capsule/src/components/Boost/Boost.tsx
+++ capsule/src/components/Boost/Boost.tsx
@@ -27,12 +27,9 @@ interface MiningData {
 }
 
 export const Boost: React.FC = () => {
-
     const [userData, setUserData] = useState<any>(null);
     const [user, setUser] = useState<UserData | null>(null);
     const [loading, setLoading] = useState(true);
-
-    // -
 
     const [currentTime, setCurrentTime] = useState<string>("");
     const [nextTime, setNextTime] = useState<string | null>(null);
@@ -44,8 +41,12 @@ export const Boost: React.FC = () => {
     const [seconds, setSecondsLeft] = useState<number>(0);
 
     const [timerFinished, setTimerFinished] = useState(false);
-
     const [value, setValue] = useState(0.000);
+
+    const [animate, setAnimate] = useState(false);
+    const [lastLevelAnimation, setLastLevelAnimation] = useState(false);
+
+    const coinsMinedSoFarRef = useRef<number>(0);
 
     useEffect(() => {
         if (window.Telegram && window.Telegram.WebApp) {
@@ -59,59 +60,144 @@ export const Boost: React.FC = () => {
         }
     }, [userData]);
 
+    useEffect(() => {
+        if (userData && userData.id) {
+            fetchMiningData(userData.id.toString());
+        }
+    }, [userData]);
+
+    useEffect(() => {
+        fetchCurrentTime();
+    }, []);
+
+    useEffect(() => {
+        const updateCountdown = () => {
+            if (nextTime && currentTime) {
+                const currentNowTime = new Date(currentTime.replace('T', ' ').replace('Z', ''));
+                const currentNextTime = new Date(nextTime.replace('T', ' ').replace('Z', ''));
+                let diffTime = currentNextTime.getTime() - currentNowTime.getTime();
+
+                if (diffTime < 0) {
+                    diffTime = 0;
+                    setTimerFinished(true);
+                }
+
+                const hours = Math.floor(diffTime / (1000 * 60 * 60));
+                const minutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+                const seconds = Math.floor((diffTime % (1000 * 60)) / 1000);
+                setHoursLeft(hours);
+                setMinutesLeft(minutes);
+                setSecondsLeft(seconds);
+            }
+        };
+
+        updateCountdown();
+
+        return () => updateCountdown();
+    }, [nextTime, currentTime]);
+
+    useEffect(() => {
+        const countdownInterval = setInterval(() => {
+            if (hours === 0 && minutes === 0 && seconds === 0) {
+                clearInterval(countdownInterval);
+                setTimerFinished(true);
+                return;
+            }
+
+            if (!timerFinished) {
+                setSecondsLeft(prevSeconds => {
+                    if (prevSeconds === 0) {
+                        setMinutesLeft(prevMinutes => {
+                            if (prevMinutes === 0) {
+                                setHoursLeft(prevHours => Math.max(0, prevHours - 1));
+                                return 59;
+                            } else {
+                                return prevMinutes - 1;
+                            }
+                        });
+                        return 59;
+                    } else {
+                        return prevSeconds - 1;
+                    }
+                });
+            }
+        }, 1000);
+
+        return () => clearInterval(countdownInterval);
+    }, [hours, minutes, seconds, timerFinished]);
+
+    useEffect(() => {
+        if (coinsMine !== null && timeMine !== null) {
+            const totalSecondsInTimeMine = timeMine * 3600;
+            const passedSeconds = (hours * 3600) + (minutes * 60) + seconds;
+            const remainingSeconds = totalSecondsInTimeMine - passedSeconds;
+
+            coinsMinedSoFarRef.current = (coinsMine * remainingSeconds) / totalSecondsInTimeMine;
+        }
+    }, [coinsMine, timeMine, hours, minutes, seconds]);
+
+    useEffect(() => {
+        let isCoinsMineSet = false;
+
+        if (coinsMine !== null && timeMine !== null) {
+            const interval = setInterval(() => {
+                const coinsPerSecond = (coinsMine / (timeMine * 3600)) / 2;
+
+                if (!isCoinsMineSet && coinsMinedSoFarRef.current === coinsMine) {
+                    setValue(coinsMinedSoFarRef.current);
+                    isCoinsMineSet = true;
+                } else {
+                    coinsMinedSoFarRef.current += coinsPerSecond;
+                    setValue(coinsMinedSoFarRef.current);
+                }
+
+                if (coinsMine === coinsMinedSoFarRef.current) {
+                    setValue(coinsMine);
+                    clearInterval(interval);
+                }
+            }, 500);
+
+            return () => clearInterval(interval);
+        }
+    }, [coinsMine, timeMine]);
+
     const fetchUserData = async (telegramUserId: string) => {
         try {
             const response = await axios.get(`https://capsule-server.onrender.com/api/user/info/${telegramUserId}`);
             setUser(response.data[0]);
         } catch (error) {
-            console.error(error);
+            console.error('Ошибка при загрузке данных пользователя:', error);
         } finally {
             setLoading(false);
         }
     };
 
-    const levels: Level[] = [
-        { id: 1, name: 'Level 1', image: 'capsule_v_1.png', coins: 100, time: 1, mines_nft: false, price: 160 },
-        { id: 2, name: 'Level 2', image: 'capsule_v_2.png', coins: 200, time: 4, mines_nft: true, price: 340 }
-    ];
-
-    const currentLevelIndex = user ? levels.findIndex(level => level.id === user.level) : -1;
-    const nextLevel = currentLevelIndex !== -1 ? levels[currentLevelIndex + 1] : null;
-    const userLevel = currentLevelIndex !== -1 ? levels[currentLevelIndex] : null;
-
-    const [animate, setAnimate] = useState(false);
-    const [lastLevelAnimation, setLastLevelAnimation] = useState(false);
-
-    const handleUpgrade = async () => {
-
-        if (nextLevel && user && user.balance >= nextLevel.price) {
-            try {
-                if (coinsMine !== null && hours <= 0 && minutes <= 0 && seconds <= 0) {
-                    await updateBalance(nextLevel.price);
-                    await updateBalanceCoins(coinsMine);
-                    await updateNextMining(nextLevel.id);
-                }
-                else {
-                    await updateBalance(nextLevel.price);
-                    await updateBalanceCoins(value);
-                    await updateNextMining(nextLevel.id);
-                }
-                await updateLevel(nextLevel.id);
-                setUser({ ...user, level: nextLevel.id, balance: user.balance - nextLevel.price });
-                if (nextLevel.id !== levels[levels.length - 1].id) {
-                    setAnimate(true);
-                    setTimeout(() => {
-                        setAnimate(false);
-                    }, 500);
-                }
-            } catch (error) {
-                console.error('Ошибка при обновлении уровня пользователя:', error);
-                alert('Произошла ошибка при обновлении уровня пользователя');
+    const fetchMiningData = async (telegramUserId: string) => {
+        try {
+            const response = await fetch(`https://capsule-server.onrender.com/api/currentMining/ready/${telegramUserId}`);
+            if (!response.ok) {
+                throw new Error('Ошибка при загрузке данных о текущей активности');
             }
-        } else if (nextLevel && user && user.balance < nextLevel.price) {
-            alert('Недостаточно средств для покупки!');
-        } else {
-            alert('Вы достигли максимального уровня!');
+            const data: MiningData = await response.json();
+            setNextTime(data.next_time);
+            setCoinsMine(data.coins_mine);
+            setTimeMine(data.time_mine);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchCurrentTime = async () => {
+        try {
+            const response = await fetch('https://capsule-server.onrender.com/api/currentTime');
+            if (!response.ok) {
+                throw new Error('Ошибка при получении текущего времени с сервера');
+            }
+            const data = await response.json();
+            const currentTimeFormatted = data.currentTime.replace(' ', 'T');
+            setCurrentTime(currentTimeFormatted);
+        } catch (error) {
+            console.error(error);
         }
     };
 
@@ -147,6 +233,16 @@ export const Boost: React.FC = () => {
         }
     };
 
+    const levels: Level[] = [
+        { id: 1, name: 'Level 1', image: 'capsule_v_1.png', coins: 100, time: 1, mines_nft: false, price: 160 },
+        { id: 2, name: 'Level 2', image: 'capsule_v_2.png', coins: 200, time: 4, mines_nft: true, price: 340 }
+    ];
+
+    const currentLevelIndex: number = user ? levels.findIndex(level => level.id === user.level) : -1;
+    const nextLevel: Level | null = currentLevelIndex !== -1 ? levels[currentLevelIndex + 1] : null;
+    const userLevel: Level | null = currentLevelIndex !== -1 ? levels[currentLevelIndex] : null;
+
+
     useEffect(() => {
         if (!nextLevel) {
             setLastLevelAnimation(true);
@@ -155,147 +251,44 @@ export const Boost: React.FC = () => {
         }
     }, [nextLevel]);
 
+    const handleUpgrade = async () => {
+        if (nextLevel && user && user.balance >= nextLevel.price) {
+            try {
+                if (coinsMine !== null && hours <= 0 && minutes <= 0 && seconds <= 0) {
+                    await updateBalance(nextLevel.price);
+                    await updateBalanceCoins(coinsMine);
+                    await updateNextMining(nextLevel.id);
+                } else {
+                    await updateBalance(nextLevel.price);
+                    await updateBalanceCoins(value);
+                    await updateNextMining(nextLevel.id);
+                }
+                await updateLevel(nextLevel.id);
+                setUser({ ...user, level: nextLevel.id, balance: user.balance - nextLevel.price });
+                if (nextLevel.id !== levels[levels.length - 1].id) {
+                    setAnimate(true);
+                    setTimeout(() => {
+                        setAnimate(false);
+                    }, 1000);
+                }
+            } catch (error) {
+                console.error('Ошибка при обновлении уровня пользователя:', error);
+                alert('Произошла ошибка при обновлении уровня пользователя');
+            }
+        } else if (nextLevel && user && user.balance < nextLevel.price) {
+            alert('Недостаточно средств для покупки!');
+        } else {
+            alert('Вы достигли максимального уровня!');
+        }
+    };
+
     if (loading) {
-        return <div></div>;
+        return <div>Загрузка...</div>;
     }
 
     if (!user) {
         return <div>Данные о пользователе не найдены.</div>;
     }
-
-    useEffect(() => {
-        if (userData && userData.id) {
-            fetchMiningData(userData.id.toString());
-        }
-    }, [userData]);
-
-    const fetchMiningData = async (telegramUserId: string) => {
-        try {
-            const response = await fetch(`https://capsule-server.onrender.com/api/currentMining/ready/${telegramUserId}`);
-            if (!response.ok) {
-                throw new Error('Ошибка при загрузке данных о текущей активности');
-            }
-            const data: MiningData = await response.json();
-            setNextTime(data.next_time);
-            setCoinsMine(data.coins_mine);
-            setTimeMine(data.time_mine);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchCurrentTime();
-    }, [])
-
-    const fetchCurrentTime = async () => {
-        try {
-            const response = await fetch('https://capsule-server.onrender.com/api/currentTime');
-            if (!response.ok) {
-                throw new Error('Ошибка при получении текущего времени с сервера');
-            }
-            const data = await response.json();
-            const currentTimeFormatted = data.currentTime.replace(' ', 'T');
-            setCurrentTime(currentTimeFormatted);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    useEffect(() => {
-        const updateCountdown = () => {
-            if (nextTime && currentTime) {
-                const currentNowTime = new Date(currentTime.replace('T', ' ').replace('Z', ''));
-                const currentNextTime = new Date(nextTime.replace('T', ' ').replace('Z', ''));
-                let diffTime = currentNextTime.getTime() - currentNowTime.getTime();
-
-                if (diffTime < 0) {
-                    diffTime = 0;
-                    setTimerFinished(true); // Установим флаг, что таймер закончился
-                }
-
-                const hours = Math.floor(diffTime / (1000 * 60 * 60));
-                const minutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
-                const seconds = Math.floor((diffTime % (1000 * 60)) / 1000);
-                setHoursLeft(hours);
-                setMinutesLeft(minutes);
-                setSecondsLeft(seconds);
-            }
-        };
-
-        updateCountdown();
-
-        return () => updateCountdown();
-    }, [nextTime, currentTime]);
-
-    useEffect(() => {
-        const countdownInterval = setInterval(() => {
-            if (hours === 0 && minutes === 0 && seconds === 0) {
-                clearInterval(countdownInterval);
-                setTimerFinished(true);
-                return;
-            }
-    
-            if (!timerFinished) {
-                setSecondsLeft(prevSeconds => {
-                    if (prevSeconds === 0) {
-                        setMinutesLeft(prevMinutes => {
-                            if (prevMinutes === 0) {
-                                setHoursLeft(prevHours => Math.max(0, prevHours - 1));
-                                return 59;
-                            } else {
-                                return prevMinutes - 1;
-                            }
-                        });
-                        return 59;
-                    } else {
-                        return prevSeconds - 1;
-                    }
-                });
-            }
-        }, 1000);
-    
-        return () => clearInterval(countdownInterval);
-    }, [hours, minutes, seconds, timerFinished]);    
-
-    const coinsMinedSoFarRef = useRef<number>(0);
-
-    useEffect(() => {
-        if (coinsMine !== null && timeMine !== null) {
-            const totalSecondsInTimeMine = timeMine * 3600;
-            const passedSeconds = (hours * 3600) + (minutes * 60) + seconds;
-            const remainingSeconds = totalSecondsInTimeMine - passedSeconds;
-
-            coinsMinedSoFarRef.current = (coinsMine * remainingSeconds) / totalSecondsInTimeMine;
-        }
-    }, [coinsMine, timeMine, hours, minutes, seconds]);
-
-    useEffect(() => {
-        let isCoinsMineSet = false; // Флаг для отслеживания установки coinsMine
-
-        if (coinsMine !== null && timeMine !== null) {
-            const interval = setInterval(() => {
-                const coinsPerSecond = (coinsMine / (timeMine * 3600)) / 2;
-
-                if (!isCoinsMineSet && coinsMinedSoFarRef.current === coinsMine) {
-                    // Установка coinsMine, если coinsMinedSoFarRef.current равен coinsMine
-                    setValue(coinsMinedSoFarRef.current);
-                    isCoinsMineSet = true; // Устанавливаем флаг в true, чтобы предотвратить повторную установку coinsMine
-                }
-                else {
-                    coinsMinedSoFarRef.current += coinsPerSecond; // добавляем coinsPerSecond к coinsMinedSoFar
-                    setValue(coinsMinedSoFarRef.current); // обновляем значение
-                }
-
-                if (coinsMine === coinsMinedSoFarRef.current) { // Проверяем, равны ли значения
-                    setValue(coinsMine);
-                    clearInterval(interval); // Останавливаем интервал
-                }
-            }, 500);
-
-            return () => clearInterval(interval);
-        }
-    }, [coinsMine, timeMine]);
 
     return (
         <div className='default-page evently-container'>
