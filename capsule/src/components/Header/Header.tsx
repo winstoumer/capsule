baa5ea553ad80fc6ee7a6029fb709adef562fd1a
winstoumer <winstoumer@proton.me>
2024-05-16 13:57:46 +0200

    modified:   capsule/src/components/Header/Header.tsx

--- capsule/src/components/Header/Header.tsx
+++ capsule/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TonConnectButton } from "@tonconnect/ui-react";
+import { TonConnectButton, TonConnectUI, THEME } from "@tonconnect/ui-react";
 import './header.scss';
 
 type TelegramUserData = {
@@ -12,9 +12,40 @@ type TelegramUserData = {
 export const Header: React.FC = () => {
     const [userData, setUserData] = useState<TelegramUserData | null>(null);
 
+    const tonConnectUI = new TonConnectUI({
+        manifestUrl: 'https://<YOUR_APP_URL>/tonconnect-manifest.json',
+        uiPreferences: {
+            colorsSet: {
+                [THEME.DARK]: {
+                    connectButton: {
+                        background: '#ffffff'
+                    }
+                }
+            }
+        }
+    });
+
     useEffect(() => {
         if (window.Telegram && window.Telegram.WebApp) {
             setUserData(window.Telegram.WebApp.initDataUnsafe?.user);
+            tonConnectUI.uiOptions = {
+                uiPreferences: {
+                    theme: THEME.DARK,
+                    borderRadius: 's',
+                    colorsSet: {
+                        [THEME.DARK]: {
+                            connectButton: {
+                                background: '#ffffff'
+                            }
+                        },
+                        [THEME.LIGHT]: {
+                            text: {
+                                primary: '#FF0000'
+                            }
+                        }
+                    }
+                }
+            };
         }
     }, []);
 
