ba32184bae2208a3e95b3968c180d4d2b4238ba6
winstoumer <winstoumer@proton.me>
2024-05-16 14:54:32 +0200

    modified:   capsule/src/components/Header/Header.tsx

--- capsule/src/components/Header/Header.tsx
+++ capsule/src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TonConnectButton } from "@tonconnect/ui-react";
+import { TonConnectButton, THEME, TonConnectUI } from "@tonconnect/ui-react";
 import './header.scss';
 
 type TelegramUserData = {
@@ -18,18 +18,62 @@ export const Header: React.FC = () => {
         }
     }, []);
 
-    return <header>
-        <div className='header-width'>
-            <div className="header-b">
-                {userData ? (
-                    <>{userData.first_name}</>
-                ) : (
-                    <span></span>
-                )}
+    // Customize TonConnect UI options
+    const tonConnectUI = new TonConnectUI({
+        uiPreferences: {
+            borderRadius: 's',
+            theme: THEME.DARK,
+            colorsSet: {
+                [THEME.DARK]: {
+                    connectButton: {
+                        background: '#29CC6A'
+                    }
+                },
+                [THEME.LIGHT]: {
+                    text: {
+                        primary: '#FF0000'
+                    }
+                }
+            }
+        }
+    });
+
+    // Example dynamic update of TonConnect UI options
+    useEffect(() => {
+        tonConnectUI.uiOptions = {
+            uiPreferences: {
+                borderRadius: 's',
+                theme: THEME.DARK,
+                colorsSet: {
+                    [THEME.DARK]: {
+                        connectButton: {
+                            background: '#29CC6A'
+                        }
+                    },
+                    [THEME.LIGHT]: {
+                        text: {
+                            primary: '#FF0000'
+                        }
+                    }
+                }
+            }
+        };
+    }, [tonConnectUI]);
+
+    return (
+        <header>
+            <div className='header-width'>
+                <div className="header-b">
+                    {userData ? (
+                        <>{userData.first_name}</>
+                    ) : (
+                        <span></span>
+                    )}
+                </div>
+                <div className="header-b">
+                    <TonConnectButton className="connect-wallet-button" />
+                </div>
             </div>
-            <div className="header-b">
-                <TonConnectButton className="connect-wallet-button" style={{ background: "#ff0000" }} />
-            </div>
-        </div>
-    </header>
+        </header>
+    );
 };
