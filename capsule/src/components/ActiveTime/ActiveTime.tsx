e24fd5ab51fa06f006bb1460d9e64d14369bdbfc
winstoumer <winstoumer@proton.me>
2024-05-18 18:13:53 +0200

    modified:   capsule/src/components/ActiveTime/ActiveTime.tsx

--- capsule/src/components/ActiveTime/ActiveTime.tsx
+++ capsule/src/components/ActiveTime/ActiveTime.tsx
@@ -14,7 +14,7 @@ interface MiningData {
 
 export const ActiveTime = () => {
     const [userData, setUserData] = useState<any>(null);
-    const [activeText, setActiveText] = useState("");
+    const [activeText, setActiveText] = useState("Active..");
     const [currentTime, setCurrentTime] = useState<string>("");
     const [nextTime, setNextTime] = useState<string | null>(null);
     const [coinsMine, setCoinsMine] = useState<number | null>(null);
@@ -49,8 +49,8 @@ export const ActiveTime = () => {
 
     function calculateTimeRemaining(currentTime: string, nftEndDate: string | null): string {
         if (nftEndDate) {
-            const nowDate = new Date(currentTime.replace('T', ' ').replace('Z', ''));
-            const endDate = new Date(nftEndDate.replace('T', ' ').replace('Z', ''));
+            const nowDate = new Date(currentTime);
+            const endDate = new Date(nftEndDate);
             const timeDiff = endDate.getTime() - nowDate.getTime();
             const oneDay = 24 * 60 * 60 * 1000;
             const oneHour = 60 * 60 * 1000;
@@ -83,7 +83,7 @@ export const ActiveTime = () => {
             setMatterId(data.matter_id);
             setNftEndDate(data.time_end_mined_nft);
             const remainingTime = calculateTimeRemaining(new Date(currentTime).toISOString(), data.time_end_mined_nft);
-            setActiveText(data.active ? `Active..` : (data.nft_active ? `Mined nft.. ${remainingTime}` : ""));
+            setActiveText(data.nft_active ? `Mined nft.. ${remainingTime}` : "");
         } catch (error) {
             console.error(error);
         }
@@ -92,7 +92,7 @@ export const ActiveTime = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             const remainingTime = calculateTimeRemaining(new Date(currentTime).toISOString(), nftEndDate);
-            setActiveText(prevText => prevText === "Active.." ? `Mined nft.. ${remainingTime}` : "");
+            setActiveText(prevText => prevText === "Active.." ? `Mined nft.. ${remainingTime}` : "Active..");
         }, 2000);
         return () => clearInterval(interval);
     }, [currentTime, nftEndDate]);
