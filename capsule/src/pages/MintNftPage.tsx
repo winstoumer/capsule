9f07e892325e9c58d9275ced305a7118256a7b9e
winstoumer <winstoumer@proton.me>
2024-05-19 22:40:22 +0200

    modified:   capsule/src/pages/MintNftPage.tsx

--- capsule/src/pages/MintNftPage.tsx
+++ capsule/src/pages/MintNftPage.tsx
@@ -15,10 +15,25 @@ interface CollectionData {
 }
 
 const MintNftPage: React.FC = () => {
+    const [userData, setUserData] = useState<any>(null);
     const { id } = useParams<{ id: string }>();
     const [collection, setCollection] = useState<CollectionData | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+
+    const [mintActive, setMintActive] = useState(false);
+
+    useEffect(() => {
+        if (window.Telegram && window.Telegram.WebApp) {
+            setUserData(window.Telegram.WebApp.initDataUnsafe?.user);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (userData && userData.id) {
+            fetchMiningData(userData.id.toString());
+        }
+    }, [userData]);
 
     useEffect(() => {
         const fetchCollection = async () => {
@@ -34,6 +49,21 @@ const MintNftPage: React.FC = () => {
 
         fetchCollection();
     }, [id]);
+
+    const fetchMiningData = async (telegramUserId: string) => {
+        try {
+            const response = await axios.get(`https://capsule-server.onrender.com/api/currentMining/current/${telegramUserId}`);
+
+            if (response.status !== 200) {
+                throw new Error('Ошибка при загрузке данных о текущей активности');
+            }
+
+            const data = response.data;
+            setMintActive(data.mint_active);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     if (loading) {
         return <div></div>;
@@ -63,15 +93,19 @@ const MintNftPage: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className='nft-description'>
-                    <div className='nft-preview-title'>
-                        You have mined <span className='color-purple'>1</span> nft
+                {mintActive ? (
+                    <div className='nft-description'>
+                        <div className='nft-preview-title'>
+                            You have mined <span className='color-purple'>1</span> nft
+                        </div>
+                        <div className='price-mint'>
+                            <span className='color-blue'>0.5</span> TON
+                        </div>
+                        <button className='default-button'>Mint</button>
                     </div>
-                    <div className='price-mint'>
-                        <span className='color-blue'>0.5</span> TON
-                    </div>
-                    <button className='default-button'>Mint</button>
-                </div>
+                ) : (
+                    <div></div>
+                )}
             </div>
         </PageComponent>
     </div>
