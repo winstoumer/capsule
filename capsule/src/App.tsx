257d2a00b92c27359c49b69438b5c003421e8c70
winstoumer <winstoumer@proton.me>
2024-05-22 00:56:14 +0200

    modified:   capsule/src/App.tsx 	modified:   capsule/src/components/PageComponent/PageComponent.tsx

--- capsule/src/App.tsx
+++ capsule/src/App.tsx
@@ -1,6 +1,7 @@
 // src/App.tsx
 
 import './App.css';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import HomePage from './pages/HomePage';
@@ -10,6 +11,23 @@ import NftPage from './pages/NftPage';
 import MintNftPage from './pages/MintNftPage';
 
 function App() {
+
+  useEffect(() => {
+    const loadScript = () => {
+      const script = document.createElement('script');
+      script.src = 'https://telegram.org/js/telegram-web-app.js';
+      script.async = true;
+      script.onload = () => {
+        if (window.Telegram && window.Telegram.WebApp) {
+          window.Telegram.WebApp.setHeaderColor('#000000');
+          window.Telegram.WebApp.expand();
+        }
+      };
+      document.body.appendChild(script);
+    };
+
+    loadScript();
+  }, []);
 
   return (
     <TonConnectUIProvider manifestUrl="https://capsule-server.onrender.com/api/ton-json/tonconnect-manifest.json"
--- capsule/src/components/PageComponent/PageComponent.tsx
+++ capsule/src/components/PageComponent/PageComponent.tsx
@@ -1,27 +1,10 @@
-import React, { useEffect, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
 interface PageComponentProps {
   children: ReactNode;
 }
 
 const PageComponent: React.FC<PageComponentProps> = ({ children }) => {
-  
-  useEffect(() => {
-    const loadScript = () => {
-      const script = document.createElement('script');
-      script.src = 'https://telegram.org/js/telegram-web-app.js';
-      script.async = true;
-      script.onload = () => {
-        if (window.Telegram && window.Telegram.WebApp) {
-          window.Telegram.WebApp.setHeaderColor('#000000');
-          window.Telegram.WebApp.expand();
-        }
-      };
-      document.body.appendChild(script);
-    };
-
-    loadScript();
-  }, []);
 
   return <>{children}</>;
 };
