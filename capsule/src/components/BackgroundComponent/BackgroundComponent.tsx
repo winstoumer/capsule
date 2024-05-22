e04743213437d17ff348d2a77eb96cc768dab27c
winstoumer <winstoumer@proton.me>
2024-05-22 15:35:26 +0200

    modified:   capsule/src/components/BackgroundComponent/BackgroundComponent.tsx

--- capsule/src/components/BackgroundComponent/BackgroundComponent.tsx
+++ capsule/src/components/BackgroundComponent/BackgroundComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import './backgroundComponent.scss';
 
 const BackgroundComponent: React.FC = () => {
   const [bubbles, setBubbles] = useState<{ left: number; top: number }[]>([]);
