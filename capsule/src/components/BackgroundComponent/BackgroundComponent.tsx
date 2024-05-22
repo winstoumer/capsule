9715997daa330a3c790b5f131093076a35fa9e3a
winstoumer <winstoumer@proton.me>
2024-05-22 15:33:02 +0200

    new file:   capsule/src/components/BackgroundComponent/BackgroundComponent.tsx 	new file:   capsule/src/components/BackgroundComponent/backgroundComponent.scss

---
+++ capsule/src/components/BackgroundComponent/BackgroundComponent.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+const BackgroundComponent: React.FC = () => {
+  const [bubbles, setBubbles] = useState<{ left: number; top: number }[]>([]);
+
+  useEffect(() => {
+    const interval = setInterval(addRandomBubble, 1000); // Добавлять новый шар каждую секунду
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const addRandomBubble = () => {
+    const randomLeft = Math.random() * window.innerWidth;
+    const randomTop = Math.random() * window.innerHeight;
+
+    setBubbles([...bubbles, { left: randomLeft, top: randomTop }]);
+  };
+
+  return (
+    <div className="background-container">
+      <div className="background">
+        {bubbles.map((bubble, index) => (
+          <div
+            key={index}
+            className="bubble"
+            style={{ left: bubble.left, top: bubble.top }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundComponent;
