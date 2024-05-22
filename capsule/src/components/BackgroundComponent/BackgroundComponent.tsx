09979c243b8455e233964d9b09c7c202c7cc935c
winstoumer <winstoumer@proton.me>
2024-05-22 15:40:16 +0200

    modified:   capsule/src/components/BackgroundComponent/BackgroundComponent.tsx 	modified:   capsule/src/components/BackgroundComponent/backgroundComponent.scss

--- capsule/src/components/BackgroundComponent/BackgroundComponent.tsx
+++ capsule/src/components/BackgroundComponent/BackgroundComponent.tsx
@@ -2,34 +2,34 @@ import React, { useEffect, useState } from 'react';
 import './backgroundComponent.scss';
 
 const BackgroundComponent: React.FC = () => {
-  const [bubbles, setBubbles] = useState<{ left: number; top: number }[]>([]);
+    const [bubbles, setBubbles] = useState<{ left: number; top: number }[]>([]);
 
-  useEffect(() => {
-    const interval = setInterval(addRandomBubble, 1000); // Добавлять новый шар каждую секунду
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const addRandomBubble = () => {
-    const randomLeft = Math.random() * window.innerWidth;
-    const randomTop = Math.random() * window.innerHeight;
-
-    setBubbles([...bubbles, { left: randomLeft, top: randomTop }]);
-  };
-
-  return (
-    <div className="background-container">
-      <div className="background">
-        {bubbles.map((bubble, index) => (
-          <div
-            key={index}
-            className="bubble"
-            style={{ left: bubble.left, top: bubble.top }}
-          ></div>
-        ))}
+    useEffect(() => {
+      const interval = setInterval(addRandomBubble, 2000); // Добавлять новый шар каждые 2 секунды
+  
+      return () => clearInterval(interval);
+    }, []);
+  
+    const addRandomBubble = () => {
+      const randomLeft = Math.random() * window.innerWidth;
+      const randomTop = Math.random() * window.innerHeight;
+  
+      setBubbles([...bubbles, { left: randomLeft, top: randomTop }]);
+    };
+  
+    return (
+      <div className="App">
+        <div className="background">
+          {bubbles.map((bubble, index) => (
+            <div
+              key={index}
+              className="bubble"
+              style={{ left: bubble.left, top: bubble.top }}
+            ></div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default BackgroundComponent;
