7b0e2d8cf6b2cbb7801c30d51c6600f0ebd5245f
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-05-28 22:24:57 +0200

    deleted:    capsule/src/components/QrCodeComponent/QrCodeComponent.tsx 	modified:   capsule/src/pages/EarnPage.tsx

--- capsule/src/pages/EarnPage.tsx
+++ capsule/src/pages/EarnPage.tsx
@@ -1,31 +1,19 @@
 // src/pages/EarnPage.tsx
 import './earn.scss';
-import React, { useState } from 'react';
+import React from 'react';
 import Tab from '../components/Tab/Tab';
 import { Navigation } from "../components/Navigation/Navigation";
 import { Earn } from "../components/Earn/Earn";
 import { Referral } from "../components/Referral/Referral";
 import PageComponent from '../components/PageComponent/PageComponent';
-import QrCodeComponent from '../components/QrCodeComponent/QrCodeComponent';
 
 const EarnPage: React.FC = () => {
-    const [text, setText] = useState('Hello, World!');
     return (
         <PageComponent>
             <Tab
                 tabs={[
                     { title: 'Earn', content: <Earn /> },
-                    { title: 'Referral', content: <Referral /> },
-                    { title: 'QrCode', content: <div>
-                        <h1>QR Code Generator</h1>
-      <input 
-        type="text" 
-        value={text} 
-        onChange={(e) => setText(e.target.value)} 
-        placeholder="Enter text for QR code" 
-      />
-      <QrCodeComponent value={text} size={256} />
-                    </div> }
+                    { title: 'Referral', content: <Referral /> }
                 ]}
             />
             <Navigation />
