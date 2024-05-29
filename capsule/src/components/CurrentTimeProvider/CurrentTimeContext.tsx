eb13c25cd0712e37ee0411025eed48cd7b241f64
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-05-30 00:30:35 +0200

    modified:   capsule/src/components/CurrentTimeProvider/CurrentTimeContext.tsx

--- capsule/src/components/CurrentTimeProvider/CurrentTimeContext.tsx
+++ capsule/src/components/CurrentTimeProvider/CurrentTimeContext.tsx
@@ -14,20 +14,24 @@ interface CurrentTimeProviderProps {
 const CurrentTimeContext = createContext<CurrentTimeContextType | undefined>(undefined);
 
 export const CurrentTimeProvider: React.FC<CurrentTimeProviderProps> = ({ children }) => {
-
     const apiUrl = import.meta.env.VITE_API_URL;
 
     const [currentTime, setCurrentTime] = useState<string | null>(null);
 
     const fetchCurrentTime = useCallback(async () => {
-        try {
-            const response = await axios.get(`${apiUrl}/api/currentTime`);
-            const currentTimeFormatted = response.data.currentTime.replace(' ', 'T');
-            setCurrentTime(currentTimeFormatted);
-        } catch (error) {
-            console.error('Ошибка при получении текущего времени с сервера:', error);
-        }
-    }, []);
+        const tryFetch = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/api/currentTime`);
+                const currentTimeFormatted = response.data.currentTime.replace(' ', 'T');
+                setCurrentTime(currentTimeFormatted);
+            } catch (error) {
+                console.error('Ошибка при получении текущего времени с сервера, повторная попытка...', error);
+                setTimeout(tryFetch, 1000); // Повторная попытка через 2 секунды
+            }
+        };
+
+        await tryFetch();
+    }, [apiUrl]);
 
     useEffect(() => {
         fetchCurrentTime();
