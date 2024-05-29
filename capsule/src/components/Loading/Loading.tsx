9e57e16599f7e86b8b1d905baf10d673b7b69272
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-05-30 00:07:54 +0200

    modified:   capsule/src/components/Loading/Loading.tsx 	modified:   capsule/src/components/Loading/loading.scss

--- capsule/src/components/Loading/Loading.tsx
+++ capsule/src/components/Loading/Loading.tsx
@@ -1,10 +1,28 @@
+// Loading.tsx
+import React from 'react';
 import './loading.scss';
 
-export const Loading = () => {
+const generateDiamonds = (numDiamonds: number) => {
+    return Array.from({ length: numDiamonds }, (_, i) => {
+        const randomX = Math.random();
+        const randomY = Math.random();
+        return (
+            <span
+                key={i}
+                className="diamond"
+                style={{ '--randomX': randomX, '--randomY': randomY } as React.CSSProperties}
+            >
+                💎
+            </span>
+        );
+    });
+};
 
+export const Loading: React.FC = () => {
     return (
-        <div className='loading-container'>
-            <div className='loading'></div>
+        <div className="loading-container">
+            {generateDiamonds(10)}
+            <div className="loading-center"></div>
         </div>
     );
 };
