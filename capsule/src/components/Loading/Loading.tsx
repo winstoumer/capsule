1f58557286359f63f4ee314fa746300625774df3
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-05-30 10:38:12 +0200

    modified:   capsule/src/components/Loading/Loading.tsx 	modified:   capsule/src/components/Loading/loading.scss

--- capsule/src/components/Loading/Loading.tsx
+++ capsule/src/components/Loading/Loading.tsx
@@ -1,42 +1,10 @@
-// Loading.tsx
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './loading.scss';
 
-interface Diamond {
-    id: number;
-    top: number;
-    left: number;
-}
-
 const Loading: React.FC = () => {
-    const [diamonds, setDiamonds] = useState<Diamond[]>([]);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setDiamonds(prevDiamonds => {
-                const newDiamond: Diamond = {
-                    id: Date.now(),
-                    top: Math.random() * 100,
-                    left: Math.random() * 100
-                };
-                return [...prevDiamonds, newDiamond];
-            });
-        }, 100); // Interval for adding new diamonds (milliseconds)
-
-        return () => clearInterval(interval);
-    }, []);
 
     return (
         <div className="loading-container">
-            {diamonds.map(diamond => (
-                <span
-                    key={diamond.id}
-                    className="diamond"
-                    style={{ top: `${diamond.top}vh`, left: `${diamond.left}vw` }}
-                >
-                    🖕
-                </span>
-            ))}
             <div className="loading-center"></div>
         </div>
     );
