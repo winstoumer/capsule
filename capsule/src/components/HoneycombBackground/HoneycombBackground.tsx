d0a79f7658dd8a94c2514f5d868c04d814a4570b
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-07 00:32:45 +0200

    modified:   capsule/src/components/HoneycombBackground/HoneycombBackground.tsx 	modified:   capsule/src/components/HoneycombBackground/honeycombBackground.module.scss

--- capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
+++ capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import styles from './honeycombBackground.module.scss';
 
 const HoneycombBackground: React.FC = () => {
-  // Calculate the number of hexagons needed to fill the screen
-  const numRows = Math.ceil(window.innerHeight / 115.47) + 2;
-  const numCols = Math.ceil(window.innerWidth / 100) + 2;
+  const rows = 10;
+  const columns = 10;
 
   return (
     <div className={styles.honeycomb}>
-      {[...Array(numRows)].map((_, rowIndex) => (
+      {[...Array(rows)].map((_, rowIndex) => (
         <div className={styles['hex-row']} key={rowIndex}>
-          {[...Array(numCols)].map((_, colIndex) => (
+          {[...Array(columns)].map((_, colIndex) => (
             <div className={styles.hex} key={`${rowIndex}-${colIndex}`}>
               <svg viewBox="0 0 100 115.47" xmlns="http://www.w3.org/2000/svg">
                 <polygon points="50,0 100,28.87 100,86.6 50,115.47 0,86.6 0,28.87" fill="none" stroke="#777" strokeWidth="1"/>
