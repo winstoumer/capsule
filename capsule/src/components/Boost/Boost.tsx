9233f99f3bb77e826b5b7d47c9cb6cea73f12abc
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-06 19:40:42 +0200

    modified:   capsule/src/components/Boost/Boost.tsx

--- capsule/src/components/Boost/Boost.tsx
+++ capsule/src/components/Boost/Boost.tsx
@@ -172,11 +172,11 @@ export const Boost: React.FC = () => {
     };
 
     const levels: Level[] = [
-        { id: 1, name: 'Level 1', image: 'capsule_1.png', coins: 10, time: 1, mines_nft: false, price: 0 },
-        { id: 2, name: 'Level 2', image: 'capsule_2.png', coins: 80, time: 4, mines_nft: false, price: 250 },
-        { id: 3, name: 'Level 3', image: 'capsule_3.png', coins: 160, time: 6, mines_nft: true, price: 1400 },
-        { id: 4, name: 'Level 4', image: 'capsule_4.png', coins: 420, time: 8, mines_nft: true, price: 3600 },
-        { id: 5, name: 'Level 5', image: 'capsule_5.png', coins: 800, time: 12, mines_nft: true, price: 7200 }
+        { id: 1, name: 'M1', image: 'capsule_1.png', coins: 10, time: 1, mines_nft: false, price: 0 },
+        { id: 2, name: 'M2', image: 'capsule_2.png', coins: 80, time: 4, mines_nft: false, price: 250 },
+        { id: 3, name: 'M3', image: 'capsule_3.png', coins: 160, time: 6, mines_nft: true, price: 1400 },
+        { id: 4, name: 'M4', image: 'capsule_4.png', coins: 420, time: 8, mines_nft: true, price: 3600 },
+        { id: 5, name: 'M5', image: 'capsule_5.png', coins: 800, time: 12, mines_nft: true, price: 7200 }
     ];
 
     const currentLevelIndex: number = level !== undefined ? levels.findIndex(l => l.id === level) : -1;
