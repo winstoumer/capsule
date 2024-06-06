adcf87ff28cd102d53b13bab1b32ead5c9a122f5
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-07 00:48:44 +0200

    modified:   capsule/src/components/HoneycombBackground/HoneycombBackground.tsx 	modified:   capsule/src/components/HoneycombBackground/honeycombBackground.module.scss

--- capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
+++ capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
@@ -15,22 +15,24 @@ const HoneycombBackground: React.FC = () => {
     };
   }, []);
 
-  const rows = Math.ceil(dimensions.height / 86.6) + 1;
-  const columns = Math.ceil(dimensions.width / 100) + 1;
+  const rows = Math.ceil(dimensions.height / 86.6);
+  const columns = Math.ceil(dimensions.width / 100);
 
   return (
-    <div className={styles.honeycomb}>
-      {[...Array(rows)].map((_, rowIndex) => (
-        <div className={styles['hex-row']} key={rowIndex}>
-          {[...Array(columns)].map((_, colIndex) => (
-            <div className={styles.hex} key={`${rowIndex}-${colIndex}`}>
-              <svg viewBox="0 0 100 115.47" xmlns="http://www.w3.org/2000/svg">
-                <polygon points="50,0 100,28.87 100,86.6 50,115.47 0,86.6 0,28.87" fill="none" stroke="#777" strokeWidth="1"/>
-              </svg>
-            </div>
-          ))}
-        </div>
-      ))}
+    <div className={styles['honeycomb-container']}>
+      <div className={styles.honeycomb}>
+        {[...Array(rows)].map((_, rowIndex) => (
+          <div className={styles['hex-row']} key={rowIndex}>
+            {[...Array(columns)].map((_, colIndex) => (
+              <div className={styles.hex} key={`${rowIndex}-${colIndex}`}>
+                <svg viewBox="0 0 100 115.47" xmlns="http://www.w3.org/2000/svg">
+                  <polygon points="50,0 100,28.87 100,86.6 50,115.47 0,86.6 0,28.87" fill="none" stroke="#777" strokeWidth="1"/>
+                </svg>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
