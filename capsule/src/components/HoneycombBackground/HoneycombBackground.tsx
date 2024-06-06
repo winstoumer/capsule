b9c132c65372be9af541596fb5263cb3603c1e9a
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-07 00:23:02 +0200

    modified:   capsule/src/components/HoneycombBackground/HoneycombBackground.tsx

--- capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
+++ capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
@@ -4,17 +4,15 @@ import styles from './honeycombBackground.module.scss';
 const HoneycombBackground: React.FC = () => {
   return (
     <div className={styles.honeycomb}>
-      <div className={styles['hex-row']}>
-        {[...Array(6)].map((_, colIndex) => (
-          <div className={styles['side-hex']} key={`left-${colIndex}`}>
-            <svg viewBox="0 0 100 57.74" xmlns="http://www.w3.org/2000/svg">
-              <polygon points="50,0 100,28.87 100,86.6 50,115.47 0,86.6 0,28.87" fill="none" stroke="#777" strokeWidth="1"/>
-            </svg>
-          </div>
-        ))}
-      </div>
       {[...Array(10)].map((_, rowIndex) => (
         <div className={styles['hex-row']} key={rowIndex}>
+          {[...Array(6)].map((_, colIndex) => (
+            <div className={styles['side-hex']} key={`left-${colIndex}`}>
+              <svg viewBox="0 0 100 57.74" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="50,0 100,28.87 100,86.6 50,115.47 0,86.6 0,28.87" fill="none" stroke="#777" strokeWidth="1"/>
+              </svg>
+            </div>
+          ))}
           {[...Array(5)].map((_, colIndex) => (
             <div className={styles.hex} key={`${rowIndex}-${colIndex}`}>
               <svg viewBox="0 0 100 115.47" xmlns="http://www.w3.org/2000/svg">
@@ -29,3 +27,4 @@ const HoneycombBackground: React.FC = () => {
 };
 
 export default HoneycombBackground;
+
