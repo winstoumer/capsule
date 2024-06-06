c2129fed2fd3db807d3b463d26ab0b13e455beeb
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-06 17:12:32 +0200

    modified:   capsule/src/components/ActiveTime/ActiveTime.tsx

--- capsule/src/components/ActiveTime/ActiveTime.tsx
+++ capsule/src/components/ActiveTime/ActiveTime.tsx
@@ -222,8 +222,8 @@ export const ActiveTime = () => {
             <div className='watch-matter'>
                 <img src={imageUrl ?? undefined} className='always-matter' />
             </div>
-            <div className='active-time' style={{ transform: 'perspective(200px) rotateX(50deg)', transformOrigin: 'top' }}>
-                <div className='current-coins'>
+            <div className='active-time'>
+                <div className='current-coins' style={{ transform: 'perspective(1000px) rotateX(30deg)', transformOrigin: 'top' }}>
                     {isInitialized ? value.toFixed(2) : null}
                 </div>
                 <div className='time-left'>
