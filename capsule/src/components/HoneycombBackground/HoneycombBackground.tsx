94356719eb00c4db9aa92e639f813350763e7a35
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-07 00:43:40 +0200

    modified:   capsule/src/components/HoneycombBackground/HoneycombBackground.tsx 	modified:   capsule/src/components/HoneycombBackground/honeycombBackground.module.scss

--- capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
+++ capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './honeycombBackground.module.scss';
 
 const HoneycombBackground: React.FC = () => {
-  const rows = Math.ceil(window.innerHeight / 86.6);
-  const columns = Math.ceil(window.innerWidth / 100) + 1;
+  const [dimensions, setDimensions] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({ width: window.innerWidth, height: window.innerHeight });
+    };
+    
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const rows = Math.ceil(dimensions.height / 86.6) + 1;
+  const columns = Math.ceil(dimensions.width / 75) + 1;
 
   return (
     <div className={styles.honeycomb}>
@@ -11,9 +24,7 @@ const HoneycombBackground: React.FC = () => {
         <div className={styles['hex-row']} key={rowIndex}>
           {[...Array(columns)].map((_, colIndex) => (
             <div className={styles.hex} key={`${rowIndex}-${colIndex}`}>
-              <svg viewBox="0 0 100 115.47" xmlns="http://www.w3.org/2000/svg">
-                <polygon points="50,0 100,28.87 100,86.6 50,115.47 0,86.6 0,28.87" fill="none" stroke="#777" strokeWidth="1"/>
-              </svg>
+              <div></div>
             </div>
           ))}
         </div>
@@ -23,4 +34,5 @@ const HoneycombBackground: React.FC = () => {
 };
 
 export default HoneycombBackground;
+
 
