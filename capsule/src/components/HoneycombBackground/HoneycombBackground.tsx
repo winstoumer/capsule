cb406eb5c03cc319a7ff34e4b30ce2401ae75f27
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-07 00:20:09 +0200

    modified:   capsule/src/components/HoneycombBackground/HoneycombBackground.tsx 	modified:   capsule/src/components/HoneycombBackground/honeycombBackground.module.scss

--- capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
+++ capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import styles from './honeycombBackground.module.scss';
+import styles from './HoneycombBackground.module.scss';
 
 const HoneycombBackground: React.FC = () => {
   return (
     <div className={styles.honeycomb}>
+      <div className={styles['hex-row']}>
+        {[...Array(6)].map((_, colIndex) => (
+          <div className={styles['side-hex']} key={`left-${colIndex}`}>
+            <svg viewBox="0 0 100 57.74" xmlns="http://www.w3.org/2000/svg">
+              <polygon points="50,0 100,28.87 100,86.6 50,115.47 0,86.6 0,28.87" fill="none" stroke="#777" strokeWidth="1"/>
+            </svg>
+          </div>
+        ))}
+      </div>
       {[...Array(10)].map((_, rowIndex) => (
         <div className={styles['hex-row']} key={rowIndex}>
           {[...Array(5)].map((_, colIndex) => (
