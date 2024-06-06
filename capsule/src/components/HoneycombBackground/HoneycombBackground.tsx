a8568286cda15267022f32f5d1a62bdca756f055
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-06 20:13:13 +0200

    modified:   capsule/src/components/HoneycombBackground/HoneycombBackground.tsx 	new file:   capsule/src/components/HoneycombBackground/honeycombBackground.module.scss 	deleted:    capsule/src/components/HoneycombBackground/honeycombBackground.scss

--- capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
+++ capsule/src/components/HoneycombBackground/HoneycombBackground.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import './honeycombBackground.scss';
+import styles from './honeycombBackground.module.scss';
 
 const HoneycombBackground: React.FC = () => {
-  return <div className='honeycomb'></div>;
+  const hexagons = Array.from({ length: 30 }, (_, index) => (
+    <div className={styles.hex} key={index}></div>
+  ));
+
+  const rows = Array.from({ length: 10 }, (_, rowIndex) => (
+    <div className={styles['hex-row']} key={rowIndex}>
+      {hexagons.slice(rowIndex * 3, rowIndex * 3 + 3)}
+    </div>
+  ));
+
+  return <div className={styles.honeycomb}>{rows}</div>;
 };
 
 export default HoneycombBackground;
