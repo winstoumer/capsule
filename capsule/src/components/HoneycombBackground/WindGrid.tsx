40dd38f920c486490cb6162979b7b38a614a8d2f
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-08 13:10:18 +0200

    modified:   capsule/src/components/HoneycombBackground/WindGrid.tsx

--- capsule/src/components/HoneycombBackground/WindGrid.tsx
+++ capsule/src/components/HoneycombBackground/WindGrid.tsx
@@ -4,54 +4,7 @@ import './WindGrid.scss';
 const WindGrid: React.FC = () => {
   return (
     <div className="northern-lights">
-      <div className="line">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-      </div>
-      <div className="line">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-      </div>
-      <div className="line">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-      </div>
-      <div className="line">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-      </div>
+      
     </div>
   );
 };
