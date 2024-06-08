0922f00230a2bc2ee86c270f888636f4d878f475
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-08 16:23:48 +0200

    modified:   capsule/src/pages/MintPage.tsx

--- capsule/src/pages/MintPage.tsx
+++ capsule/src/pages/MintPage.tsx
@@ -129,7 +129,7 @@ const MintPage: React.FC = () => {
         messages: [
             {
                 address: 'UQAhCS5LHKEXQra40dGwZ7TE0jW8ilu8GBtIVV9o3Kg0nZWE',
-                amount: '20000000',
+                amount: '200000000',
             },
         ],
     };
