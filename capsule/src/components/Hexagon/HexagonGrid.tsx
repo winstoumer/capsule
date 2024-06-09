afe815e22f2c34bf9621c7c7ce825c7bbd7a2ee9
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-06-09 17:26:42 +0200

    modified:   capsule/src/components/Hexagon/HexagonGrid.tsx

--- capsule/src/components/Hexagon/HexagonGrid.tsx
+++ capsule/src/components/Hexagon/HexagonGrid.tsx
@@ -8,16 +8,24 @@ const HexagonGrid: React.FC = () => {
   const fill = '#3498db'; // Цвет шестиугольника
   const hexagons = [];
 
-  const rows = Math.ceil(window.innerHeight / (Math.sqrt(3) * size));
-  const cols = Math.ceil(window.innerWidth / (1.5 * size));
+  const diameter = Math.min(window.innerHeight, window.innerWidth) * 0.9;
+  const radius = diameter / 2;
+  const rows = Math.ceil(diameter / (Math.sqrt(3) * size));
+  const cols = Math.ceil(diameter / (1.5 * size));
 
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
       const x = col * size * 1.5;
       const y = row * Math.sqrt(3) * size + (col % 2 ? Math.sqrt(3) * 0.5 * size : 0);
-      hexagons.push(<g key={`${row}-${col}`} transform={`translate(${x},${y})`}>
-        <Hexagon size={size} fill={fill} />
-      </g>);
+      const dx = x - radius;
+      const dy = y - radius;
+      if (dx * dx + dy * dy < radius * radius) {
+        hexagons.push(
+          <g key={`${row}-${col}`} transform={`translate(${x},${y})`}>
+            <Hexagon size={size} fill={fill} />
+          </g>
+        );
+      }
     }
   }
 
