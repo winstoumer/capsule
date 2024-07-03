1b62554c0a860031a031973d29d1ee5d7c565e01
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-03 12:54:25 +0200

    modified:   capsule/src/components/Game/Game.tsx

--- capsule/src/components/Game/Game.tsx
+++ capsule/src/components/Game/Game.tsx
@@ -3,7 +3,7 @@ import './game.scss';
 
 const Game: React.FC = () => {
     const [coins, setCoins] = useState<number>(0);
-    const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
+    const [clicks, setClicks] = useState<{ id: number; x: number; y: number }[]>([]);
     const [nextId, setNextId] = useState<number>(0);
     const [gameStarted, setGameStarted] = useState<boolean>(false);
     const [timeLeft, setTimeLeft] = useState<number>(10);
@@ -21,7 +21,7 @@ const Game: React.FC = () => {
         setNextId(nextId + 1);
 
         setTimeout(() => {
-            setClicks(currentClicks => currentClicks.filter(click => click.id !== nextId));
+            setClicks((currentClicks) => currentClicks.filter((click) => click.id !== nextId));
         }, 1000);
     };
 
@@ -38,58 +38,61 @@ const Game: React.FC = () => {
     };
 
     useEffect(() => {
-        if (gameStarted && timeLeft > 0) {
-            const timer = setTimeout(() => {
-                setTimeLeft(timeLeft - 1);
+        let timer: NodeJS.Timeout | null = null;
+
+        if (gameStarted) {
+            timer = setTimeout(() => {
+                setTimeLeft((prevTimeLeft) => {
+                    const newTimeLeft = prevTimeLeft - 1;
+                    if (newTimeLeft === 0) {
+                        setShowClaimButton(true);
+                        setGameStarted(false);
+                    }
+                    return newTimeLeft;
+                });
             }, 1000);
-            return () => clearTimeout(timer);
-        } else if (timeLeft === 0 && gameStarted) {
-            setShowClaimButton(true);
-            setGameStarted(false);
         }
+
+        return () => {
+            if (timer) clearTimeout(timer);
+        };
     }, [gameStarted, timeLeft]);
 
     return (
         <div className="game">
-            {!gameStarted && (
-                <button className={`start-button default-button ${gameStarted ? 'fade-out' : ''}`} onClick={handleStartClick}>
+            {!gameStarted && !showClaimButton && (
+                <button className="start-button default-button" onClick={handleStartClick}>
                     Start
                 </button>
             )}
             {gameStarted && (
                 <>
-                    <div className='coins-container'>
-                        <div className='coins'>{coins}</div>
+                    <div className="coins-container">
+                        <div className="coins">{coins}</div>
                     </div>
-                    {timeLeft > 0 ? (
-                        <button className='button-game' onClick={handleButtonClick}>
-                            <svg width="230" height="230" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="115" cy="115" r="110" stroke="white" strokeWidth="1" fill="none" />
-                            </svg>
-                            {clicks.map(click => (
-                                <div
-                                    key={click.id}
-                                    className="floating-number"
-                                    style={{ left: click.x, top: click.y }}
-                                >
-                                    1
-                                </div>
-                            ))}
-                        </button>
-                    ) : (
-                        showClaimButton && (
-                            <button className="claim-button default-button" onClick={handleClaimClick}>
-                                Claim
-                            </button>
-                        )
-                    )}
-                    <div className='progress-bar-container'>
-                        <div
-                            className='progress-bar'
-                            style={{ width: `${(timeLeft / 10) * 100}%` }}
-                        />
+                    <button className="button-game" onClick={handleButtonClick}>
+                        <svg width="230" height="230" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="115" cy="115" r="110" stroke="white" strokeWidth="1" fill="none" />
+                        </svg>
+                        {clicks.map((click) => (
+                            <div
+                                key={click.id}
+                                className="floating-number"
+                                style={{ left: click.x, top: click.y }}
+                            >
+                                1
+                            </div>
+                        ))}
+                    </button>
+                    <div className="progress-bar-container">
+                        <div className="progress-bar" style={{ width: `${(timeLeft / 10) * 100}%` }} />
                     </div>
                 </>
+            )}
+            {!gameStarted && showClaimButton && (
+                <button className="claim-button default-button" onClick={handleClaimClick}>
+                    Claim
+                </button>
             )}
         </div>
     );
