c5cb941acfeaa84ec035fe871c44aa972d34e915
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-03 12:04:08 +0200

    modified:   capsule/src/components/Game/Game.tsx 	modified:   capsule/src/components/Game/game.scss

--- capsule/src/components/Game/Game.tsx
+++ capsule/src/components/Game/Game.tsx
@@ -3,9 +3,21 @@ import './game.scss';
 
 const Game: React.FC = () => {
     const [coins, setCoins] = useState<number>(0);
+    const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
+    const [nextId, setNextId] = useState<number>(0);
 
-    const handleButtonClick = () => {
+    const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
         setCoins(coins + 1);
+        setClicks([...clicks, { id: nextId, x, y }]);
+        setNextId(nextId + 1);
+
+        setTimeout(() => {
+            setClicks(currentClicks => currentClicks.filter(click => click.id !== nextId));
+        }, 1000);
     };
 
     return (
@@ -13,8 +25,17 @@ const Game: React.FC = () => {
             <div className='coins'>{coins}</div>
             <button className='button-game' onClick={handleButtonClick}>
                 <svg width="230" height="230" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="115" cy="115" r="110" stroke="white" stroke-width="1" fill="none" />
+                    <circle cx="115" cy="115" r="110" stroke="white" strokeWidth="1" fill="none" />
                 </svg>
+                {clicks.map(click => (
+                    <div
+                        key={click.id}
+                        className="floating-number"
+                        style={{ left: click.x, top: click.y }}
+                    >
+                        1
+                    </div>
+                ))}
             </button>
         </div>
     );
