1b1315b6abcbb0bf3592d4944384f6e7c6975a82
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-04 17:36:35 +0200

    modified:   capsule/src/pages/GamePage.tsx

--- capsule/src/pages/GamePage.tsx
+++ capsule/src/pages/GamePage.tsx
@@ -10,7 +10,7 @@ const GamePage: React.FC = () => {
       <Game
         duration={30}
         coinsPerClick={0.1}
-        maxTouches={1}
+        maxTouches={4}
         multiplier={false}
       />
     </PageComponent>
