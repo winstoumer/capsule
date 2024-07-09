ba2b101d9e7f30331db33ebfe3a5bb517a4c495b
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-09 16:57:37 +0200

    modified:   capsule/src/components/Game/Game.tsx

--- capsule/src/components/Game/Game.tsx
+++ capsule/src/components/Game/Game.tsx
@@ -191,7 +191,7 @@ const Game: React.FC<GameProps> = ({ duration, coinsPerClick, maxTouches, multip
                                     <circle cx="140" cy="140" r="70" fill="black" />
                                     {/* Анимированное сияние */}
                                     <circle cx="140" cy="140" r="140" fill="url(#glowGradient)">
-                                        <animate attributeName="r" dur="10s" values="100; 230; 100" repeatCount="indefinite" />
+                                        <animate attributeName="r" dur="10s" values="100; 220; 100" repeatCount="indefinite" />
                                         <animate attributeName="opacity" dur="2s" values="1; 0.5; 1" repeatCount="indefinite" />
                                         <animateTransform attributeName="transform" type="rotate" dur="4s" from="0 140 140" to="360 140 140" repeatCount="indefinite" />
                                     </circle>
