75c26b28f0e95ae19467bc74de1ba9b49654d9c8
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-13 12:45:28 +0200

    modified:   capsule/src/pages/HomePage.tsx

--- capsule/src/pages/HomePage.tsx
+++ capsule/src/pages/HomePage.tsx
@@ -28,6 +28,26 @@ const SvgLogo = (
   </svg>
 );
 
+const SvgMatter = (
+  <svg width="280" height="280" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 280 280">
+    <defs>
+      <radialGradient id="glowGradient" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+        <stop offset="0%" style={{ stopColor: '#00CDFF', stopOpacity: 1 }} />
+        <stop offset="70%" style={{ stopColor: 'transparent', stopOpacity: 0.5 }} />
+        <stop offset="100%" style={{ stopColor: 'transparent', stopOpacity: 0 }} />
+      </radialGradient>
+    </defs>
+    <rect width="100%" height="100%" fill="transparent" />
+    <circle cx="140" cy="140" r="70" fill="black" />
+    <circle cx="140" cy="140" r="140" fill="url(#glowGradient)">
+      <animate attributeName="r" dur="10s" values="100; 100; 220" repeatCount="indefinite" />
+      <animate attributeName="opacity" dur="2s" values="1; 0.5; 1" repeatCount="indefinite" />
+      <animateTransform attributeName="transform" type="rotate" dur="4s" from="140 100 140" to="360 100 140" repeatCount="indefinite" />
+    </circle>
+    <circle cx="140" cy="140" r="34" fill="black" />
+  </svg>
+);
+
 const HomePage: React.FC = () => {
   const { balanceData, loading } = useData();
 
@@ -37,7 +57,7 @@ const HomePage: React.FC = () => {
 
   const items = [
     { logo: SvgLogo, buttonText: 'Open', link: '/' },
-    { logo: SvgLogo, buttonText: 'Mining', link: '/mining' }
+    { logo: SvgMatter, buttonText: 'Mining', link: '/mining' }
   ];
 
   return (
