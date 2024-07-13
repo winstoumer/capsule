7f26f708b325c80c9b3883148dc6051ab906f770
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-13 13:32:32 +0200

    modified:   capsule/src/pages/HomePage.tsx

--- capsule/src/pages/HomePage.tsx
+++ capsule/src/pages/HomePage.tsx
@@ -50,7 +50,7 @@ const SvgMatter = (
       />
       <animate attributeName="opacity" dur="2s" values="1; 0.5; 1" repeatCount="indefinite" />
     </circle>
-    <circle cx="140" cy="140" r="34" fill="black" />
+    <circle cx="140" cy="140" r="40" fill="black" />
   </svg>
 );
 
