a9f6c8ef6b58d1e3ae7b16348ef6d8793be26a9d
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-13 12:13:09 +0200

    modified:   capsule/src/components/Earn/Earn.tsx

--- capsule/src/components/Earn/Earn.tsx
+++ capsule/src/components/Earn/Earn.tsx
@@ -69,21 +69,23 @@ export const Earn = () => {
     }
 
     const Completed = () => (
-        <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <path
-                d="M5 12L10 17L19 8"
-                stroke="white"
-                strokeWidth="0.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-        </svg>
+        <span className='arrow-button'>
+            <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M5 12L10 17L19 8"
+                    stroke="white"
+                    strokeWidth="0.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+            </svg>
+        </span>
     );
 
     return (
