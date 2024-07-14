7008965b41be1b5473694dbfac4d629fec6a413f
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-14 18:07:00 +0200

    modified:   capsule/src/components/Navigation/Navigation.tsx 	modified:   capsule/src/index.css

--- capsule/src/components/Navigation/Navigation.tsx
+++ capsule/src/components/Navigation/Navigation.tsx
@@ -1,21 +1,38 @@
-import { Link } from 'react-router-dom';
+// navigation.tsx
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './navigation.scss';
 
-export const Navigation = () => {
+export const Navigation: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path: string) => {
+    const contentElement = document.querySelector('.content');
+
+    if (contentElement) {
+      contentElement.classList.add('slideDown');
+      setTimeout(() => {
+        navigate(path);
+      }, 800); // Задержка должна соответствовать длительности анимации
+    } else {
+      navigate(path);
+    }
+  };
 
   return (
     <div className='bottom-navigation'>
       <div className='navigation'>
-        <div className="nav-b">
-          <Link to="/earn">Tasks</Link>
+        <div className="nav-b" onClick={() => handleNavigation('/earn')}>
+          Tasks
         </div>
-        <div className="nav-b">
-          <Link to="/frens">Frens</Link>
+        <div className="nav-b" onClick={() => handleNavigation('/frens')}>
+          Frens
         </div>
-        <div className="nav-b">
-          <Link className='color-nft' to="/collections">Nft</Link>
+        <div className="nav-b color-nft" onClick={() => handleNavigation('/collections')}>
+          Nft
         </div>
       </div>
     </div>
   );
 };
+
