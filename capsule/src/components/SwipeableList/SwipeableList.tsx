877ba97bbaddae6edd1b27470af5b8e40f585225
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-26 19:50:57 +0200

    modified:   capsule/src/components/SwipeableList/SwipeableList.tsx

--- capsule/src/components/SwipeableList/SwipeableList.tsx
+++ capsule/src/components/SwipeableList/SwipeableList.tsx
@@ -16,13 +16,13 @@ interface SwipeableListProps {
 
 const PreviousArrow = () => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M15 19L8 12L15 5" stroke="white" strokeWidth="0.5" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M15 19L8 12L15 5" stroke="#CF00F8" strokeWidth="0.5" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 );
 
 const NextArrow = () => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9 5L16 12L9 19" stroke="white" strokeWidth="0.5" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M9 5L16 12L9 19" stroke="#CF00F8" strokeWidth="0.5" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 );
 
