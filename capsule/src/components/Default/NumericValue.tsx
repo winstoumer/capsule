17ef81158c6cf74cff480009f463d023a17ab981
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-07-29 19:46:42 +0200

    modified:   capsule/src/components/Default/NumericValue.tsx

--- capsule/src/components/Default/NumericValue.tsx
+++ capsule/src/components/Default/NumericValue.tsx
@@ -1,25 +1,29 @@
-import 'react';
+import React from 'react';
 
 interface NumericValueProps {
-  value: number | null | undefined;
+  value: number | string | null | undefined;
   decimalPlaces?: number;
 }
 
 const NumericValue: React.FC<NumericValueProps> = ({ value, decimalPlaces = 0 }) => {
   let formattedValue: string = 'N/A';
 
+  // Try to parse the value to a number if it's a string
+  if (typeof value === 'string') {
+    value = parseFloat(value.replace(/,/g, ''));
+  }
+
+  // Check if the value is a valid number
   if (typeof value === 'number' && !isNaN(value)) {
     formattedValue = value.toLocaleString(undefined, {
       minimumFractionDigits: decimalPlaces,
-      maximumFractionDigits: decimalPlaces
+      maximumFractionDigits: decimalPlaces,
     });
   } else {
-    console.warn('Invalid value for NumericValue:', value); // Warning for unexpected value types
+    console.warn('NumericValue received invalid number:', value, 'Type:', typeof value);
   }
 
-  return (
-    <span>{formattedValue}</span>
-  );
+  return <span>{formattedValue}</span>;
 };
 
 export default NumericValue;
